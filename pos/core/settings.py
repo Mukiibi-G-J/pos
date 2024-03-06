9f63e73ad86220f3039dc77915cc98abb7555d65
Mukiibi-G-J <mukiibijosephgilbert865@gmail.com>
2024-03-07 00:43:09 +0300

    static files

--- pos/core/settings.py
+++ pos/core/settings.py
@@ -132,7 +132,7 @@ APPEND_SLASH = False
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-STATIC_URL = "static/"
+STATIC_URL = "static_files/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
