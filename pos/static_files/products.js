8065ef33897ab25c364e6243a51d893775620c98
Mukiibi-G-J <mukiibijosephgilbert865@gmail.com>
2024-03-07 00:47:27 +0300

    static files

--- pos/static_files/products.js
+++
@@ -1,8 +0,0 @@
-const edit_product_button = document.getElementById("edit_product_button");
-
-edit_product_button.addEventListener("click", function (event){
-    event.preventDefault()
-    console.log("edit button was clicked")
-})
-
- console.log(edit_product_button)
