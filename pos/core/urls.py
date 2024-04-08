b8196d6feb1f4075c8a5634a9a8577c1c961a18a
Mukiibi-G-J <mukiibijosephgilber865@gmail.com>
2024-04-08 21:43:46 +0300

    static files line

--- pos/core/urls.py
+++ pos/core/urls.py
@@ -18,5 +18,7 @@ urlpatterns += [path("api/", include("api.urls", namespace="api"))]
 
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
     
 
